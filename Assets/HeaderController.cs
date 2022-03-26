c3c9f128c0b605e727a2072f5f6f2460c1346e13
Valentin <valent1n.krylov@yandex.ru>
2022-03-26 19:23:02 +0300

    Create authorization panel

---
+++ Assets/HeaderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeaderController : MonoBehaviour
+{
+    [SerializeField] private Button _logInButton;
+    [SerializeField] private Button _signUpButton;
+
+    [SerializeField] private SelectionBarTween _selectionBarTween;
+    
+    private void OnEnable()
+    {
+        _logInButton.onClick.AddListener(ChangeAuthorizationPanel);
+        _signUpButton.onClick.AddListener(ChangeAuthorizationPanel);
+    }
+    
+    private void OnDisable()
+    {
+        _logInButton.onClick.RemoveListener(ChangeAuthorizationPanel);
+        _signUpButton.onClick.RemoveListener(ChangeAuthorizationPanel);
+    }
+
+    private void ChangeAuthorizationPanel()
+    {
+        _selectionBarTween.Move();
+    }
+}
---
+++ Assets/SwipePanel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipePanel : MonoBehaviour
+{
+    //[SerializeField] private Transform _panelPosition;
+    public Transform PanelPosition { get; private set; }
+
+    private void Awake()
+    {
+        PanelPosition = transform;
+    }
+}
